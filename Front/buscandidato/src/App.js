1de2048ab4267e9acddd7ed3f7281dbd22d0c2bf
eijiuchiyama <eijiuchiyama@usp.br>
2024-09-08 23:11:26 -0300

    Adicionado bootstrap

--- Front/buscandidato/src/App.js
+++ Front/buscandidato/src/App.js
@@ -3,22 +3,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <html>
+      <head>
+      <link href=" https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css " rel="stylesheet"></link>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+      </head>
+      <body>
+        <div class="container-fluid p-5 text-center">
+          <h1>BusCandidato</h1>
+        </div>
+      </body>
+    </html>
   );
 }
 
