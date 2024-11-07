98f3f746fba834a39c81d9810e4961d6a87eb836
Fernando Yang <hagisf@usp.br>
2024-11-07 16:16:51 -0300

    Refatorar admin.py

--- Back/buscandidatoapp/admin.py
+++ Back/buscandidatoapp/admin.py
@@ -1,30 +1,17 @@
 from django.contrib import admin
-from .models import Politico
-from .models import Partido
-from .models import Orgao
-from .models import Votacao
-from .models import Frente
-from .models import Proposicao
-from .models import Despesa
-from .models import Mandato
-from .models import Integrante_Partido
-from .models import Votacao_Politico
-from .models import Votacao_Proposicao
-from .models import Autor_Proposicao
-from .models import Integrante_Orgao
-from .models import Integrante_Frente
+from . import models
 
-admin.site.register(Politico)
-admin.site.register(Partido)
-admin.site.register(Orgao)
-admin.site.register(Votacao)
-admin.site.register(Frente)
-admin.site.register(Proposicao)
-admin.site.register(Despesa)
-admin.site.register(Mandato)
-admin.site.register(Integrante_Partido)
-admin.site.register(Votacao_Politico)
-admin.site.register(Votacao_Proposicao)
-admin.site.register(Autor_Proposicao)
-admin.site.register(Integrante_Orgao)
-admin.site.register(Integrante_Frente)
+admin.site.register(models.Politico)
+admin.site.register(models.Partido)
+admin.site.register(models.Orgao)
+admin.site.register(models.Votacao)
+admin.site.register(models.Frente)
+admin.site.register(models.Proposicao)
+admin.site.register(models.Despesa)
+admin.site.register(models.Mandato)
+admin.site.register(models.Integrante_Partido)
+admin.site.register(models.Votacao_Politico)
+admin.site.register(models.Votacao_Proposicao)
+admin.site.register(models.Autor_Proposicao)
+admin.site.register(models.Integrante_Orgao)
+admin.site.register(models.Integrante_Frente)
