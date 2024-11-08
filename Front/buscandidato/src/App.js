5397748bebf57f38fa73bb584a28689cb8bddea1
eijiuchiyama <eijiuchiyama@usp.br>
2024-11-07 21:09:35 -0300

    Página inicial parcialmente responsiva

--- Front/buscandidato/src/App.js
+++ Front/buscandidato/src/App.js
@@ -11,6 +11,7 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import SobreSite from './SobreSite';
 import { Link } from "react-router-dom";
+import { useMediaQuery } from 'react-responsive';
 
 
 const MenuIcon = () => {
@@ -18,11 +19,14 @@ const MenuIcon = () => {
 }
 
 function SearchBarContent(){
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+  const isDesktop = useMediaQuery({ minWidth: 768 });
   return (
     <>
+      {isDesktop &&
       <div class="container">
         <div class="row">
-          <div class="col-1">
+          <div class="col-1 mb-2">
             <DropdownMenu optionsList={["Candidato", "Partido", "Casa Legislativa"]} >
               <h7>Tags</h7>
             </DropdownMenu>
@@ -33,49 +37,110 @@ function SearchBarContent(){
               />
           </div>
         </div>
-      </div>
+      </div>}
+      {isMobile &&
+      <div class="container">
+        <div class="text-center">
+          <div class="mb-3">
+            <DropdownMenu optionsList={["Candidato", "Partido", "Casa Legislativa"]} >
+              <h7>Tags</h7>
+            </DropdownMenu>
+          </div>
+          <div>
+            <Form.Control
+                placeholder="Procure por um candidato ou político"
+              />
+          </div>
+        </div>
+      </div>}
     </>
   );
 }
 
 function SecondSectionButtons(){
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+  const isDesktop = useMediaQuery({ minWidth: 768 });
   return (
     <>
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col text-center"> 
+      {isDesktop &&
+      <div class="container-flex">
+        <div class="row">
+          <div class="col p-3 text-center"> 
           <Link to='/municipal'><Button>Câmara SP</Button></Link>
-            <div class="container p-3 m-3 rounded" style={{backgroundColor: '#55ff55'}}>
+            <div class="container p-3 mt-3 rounded" style={{backgroundColor: '#55ff55'}}>
               <img src={camaraMunicipalIcon} class="img-fluid"></img>
             </div>
           </div>
-          <div class="col text-center"> 
+          <div class="col p-3 text-center"> 
           <Link to='/deputados'><Button>Câmara dos Deputados</Button></Link> 
-            <div class="container p-3 m-3 rounded" style={{backgroundColor: '#5555ff'}}>
+            <div class="container p-3 mt-3 rounded" style={{backgroundColor: '#5555ff'}}>
               <img src={camaraDeputadosIcon} class="img-fluid"></img>
             </div>
           </div>
-          <div class="col text-center"> 
+          <div class="col p-3 text-center"> 
             <Link to='/senado'><Button>Senado</Button></Link>
-            <div class="container p-3 m-3 rounded" style={{backgroundColor: '#ff5555'}}>
+            <div class="container p-3 mt-3 rounded" style={{backgroundColor: '#ff5555'}}>
               <img src={senadoIcon} class="img-fluid"></img>
             </div>
           </div>
         </div>
-      </div>
+      </div>}
+      {isMobile &&
+      <div class="container-flex">
+        <div class="justify-content-center">
+          <div class="text-center"> 
+          <Link to='/municipal'><Button>Câmara SP</Button></Link>
+            <div class="container p-3 mb-5 mt-3 rounded" style={{backgroundColor: '#55ff55'}}>
+              <img src={camaraMunicipalIcon} class="img-fluid"></img>
+            </div>
+          </div>
+          <div class="text-center"> 
+          <Link to='/deputados'><Button>Câmara dos Deputados</Button></Link> 
+            <div class="container p-3 mb-5 mt-3 rounded" style={{backgroundColor: '#5555ff'}}>
+              <img src={camaraDeputadosIcon} class="img-fluid"></img>
+            </div>
+          </div>
+          <div class="text-center"> 
+            <Link to='/senado'><Button>Senado</Button></Link>
+            <div class="container p-3 mb-5 mt-3 rounded" style={{backgroundColor: '#ff5555'}}>
+              <img src={senadoIcon} class="img-fluid"></img>
+            </div>
+          </div>
+        </div>
+      </div>}
     </>
   );
 }
 
 export function Header(){
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+  const isDesktop = useMediaQuery({ minWidth: 768 });
   return (
-    <div class="container m-4">
-      <div class="d-flex align-items-center justify-content-between">
-        <div class="d-flex flex-grow-1"></div>
-        <div class="text-center">
+    <>
+      {isDesktop && 
+      <div class="container m-4">
+        <div class="d-flex align-items-center justify-content-between">
+          <div class="d-flex flex-grow-1"></div>
+          <div class="text-center m-4">
+            <Link to='/'><img src={logo} class="img-fluid" alt="BusCandidatoLogo"></img></Link>
+          </div>
+          <div class="d-flex flex-grow-1 justify-content-end m-5">
+            <DropdownMenu optionsList={[
+              <Link to='/' style={{color: "#000000", textDecoration: "none"}}>Página Inicial</Link>, 
+              <Link to='/candidatos' style={{color: "#000000", textDecoration: "none"}}>Todos os Políticos</Link>, 
+              <Link to='/partidos' style={{color: "#000000", textDecoration: "none"}}>Todos os Partidos</Link>, 
+              <Link to="/atualizacoes" style={{color: "#000000", textDecoration: "none"}}>Atualizações Recentes</Link>, 
+              <Link to='/sobre' style={{color: "#000000", textDecoration: "none"}}>Sobre o site</Link>]}> <MenuIcon/> </DropdownMenu>
+          </div>
+        </div>
+      </div>}
+      {isMobile && 
+      <div class="container-flex">
+      <div class="text-center">
+        <div class="m-2">
           <Link to='/'><img src={logo} class="img-fluid" alt="BusCandidatoLogo"></img></Link>
         </div>
-        <div class="d-flex flex-grow-1 justify-content-end m-5">
+        <div class="m-5">
           <DropdownMenu optionsList={[
             <Link to='/' style={{color: "#000000", textDecoration: "none"}}>Página Inicial</Link>, 
             <Link to='/candidatos' style={{color: "#000000", textDecoration: "none"}}>Todos os Políticos</Link>, 
@@ -84,12 +149,14 @@ export function Header(){
             <Link to='/sobre' style={{color: "#000000", textDecoration: "none"}}>Sobre o site</Link>]}> <MenuIcon/> </DropdownMenu>
         </div>
       </div>
-    </div>
-  
+    </div>}
+    </>
   );
 }
 
 function App() {
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+  const isDesktop = useMediaQuery({ minWidth: 768 });
   return (
     <html>
       <head>
@@ -97,22 +164,40 @@ function App() {
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
       <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
       </head>
+      {isDesktop &&
       <body class="container p-3" style={{backgroundColor: '#d8d8d8'}}>
         <Header />
         <div class="container rounded p-3">
           <div class="row"> <h1 class="text-center"></h1> </div>
-          <div class="row m-5">
+          <div class="row">
             <ContentBox header="Digite o nome de um candidato ou partido">
               <SearchBarContent />
             </ContentBox>
           </div>
-          <div class="row m-5">
+          <div class="row">
             <ContentBox header="Ou escolha uma casa legislativa">
               <SecondSectionButtons />
             </ContentBox>
           </div>
         </div>
-      </body>
+      </body>}
+      {isMobile &&
+      <body class="container p-3" style={{backgroundColor: '#d8d8d8'}}>
+        <Header />
+        <div class="container rounded p-3">
+          <div class="row"> <h1 class="text-center"></h1> </div>
+          <div class="row">
+            <ContentBox header="Digite o nome de um candidato ou partido">
+              <SearchBarContent />
+            </ContentBox>
+          </div>
+          <div class="row">
+            <ContentBox header="Ou escolha uma casa legislativa">
+              <SecondSectionButtons />
+            </ContentBox>
+          </div>
+        </div>
+      </body>}
     </html>
   );
 }
