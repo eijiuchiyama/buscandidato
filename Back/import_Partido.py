8fe19ba5cef7630fe3e16aba511b944ae10c87f6
Fernando Yang <hagisf@usp.br>
2024-11-12 15:24:40 -0300

    Corrigir bug do arquivo import incluir dados já existentes

--- Back/import_Partido.py
+++ Back/import_Partido.py
@@ -12,33 +12,39 @@ with open("partidos.json", "r") as file:
     data = json.load(file)
 
 for entry in data:
-    partido = models.Partido(
-        Sigla_Partido = entry["sigla"],
-        Nome = entry["nome"],
-    )
-    if entry["detalhes"] != None:
-        partido.Numero_Eleitoral = entry["detalhes"]["numeroEleitoral"]
-        if entry["detalhes"]["status"] != None:
-            partido.Qty_Membros_Camara = entry["detalhes"]["status"]["totalMembros"]
-            if entry["detalhes"]["status"]["lider"] != None and entry["detalhes"]["status"]["lider"]["uri"] != "":
-                politico_data = requests.get(entry["detalhes"]["status"]["lider"]["uri"]).json()["dados"]
-                politico = models.Politico(
-                    CPF = politico_data["cpf"],
-                    Nome = politico_data["nomeCivil"],
-                    Partido_Atual = politico_data["ultimoStatus"]["siglaPartido"],
-                    Estado = politico_data["ultimoStatus"]["siglaUf"],
-                    Estado_Nascimento = politico_data["ufNascimento"],
-                    Municipio_Nascimento = politico_data["municipioNascimento"],
-                    Data_Nascimento = politico_data["dataNascimento"],
-                    Sexo = politico_data["sexo"],
-                    Escolaridade = politico_data["escolaridade"],
-                    Telefone = politico_data["ultimoStatus"]["gabinete"]["telefone"],
-                    Email = politico_data["ultimoStatus"]["gabinete"]["email"],
-                )
-                politico.save()
+    if not models.Partido.objects.filter(Sigla_Partido = entry["sigla"]).exists():
+        partido = models.Partido(
+            Sigla_Partido = entry["sigla"],
+            Nome = entry["nome"],
+        )
+        if entry["detalhes"] != None:
+            partido.Numero_Eleitoral = entry["detalhes"]["numeroEleitoral"]
+            if entry["detalhes"]["status"] != None:
+                partido.Qty_Membros_Camara = entry["detalhes"]["status"]["totalMembros"]
+                if entry["detalhes"]["status"]["lider"] != None and entry["detalhes"]["status"]["lider"]["uri"] != "":
+                    politico_data = requests.get(entry["detalhes"]["status"]["lider"]["uri"]).json()["dados"]
+                    
+                    if not models.Politico.objects.filter(CPF = politico_data["cpf"]).exists():
+                        politico = models.Politico(
+                            CPF = politico_data["cpf"],
+                            Nome = politico_data["nomeCivil"],
+                            Partido_Atual = politico_data["ultimoStatus"]["siglaPartido"],
+                            Estado = politico_data["ultimoStatus"]["siglaUf"],
+                            Estado_Nascimento = politico_data["ufNascimento"],
+                            Municipio_Nascimento = politico_data["municipioNascimento"],
+                            Data_Nascimento = politico_data["dataNascimento"],
+                            Sexo = politico_data["sexo"],
+                            Escolaridade = politico_data["escolaridade"],
+                            Telefone = politico_data["ultimoStatus"]["gabinete"]["telefone"],
+                            Email = politico_data["ultimoStatus"]["gabinete"]["email"],
+                        )
+                        politico.save()
+                        print(politico.CPF + " information updated.")
 
-                partido.Lider_Partido_CPF = politico
-    partido.save()
+                    politico = models.Politico.objects.get(CPF = politico_data["cpf"])
+                    partido.Lider_Partido_CPF = politico
+        partido.save()
+        print(partido.Sigla_Partido + " information updated.")
 
 print("Data successfully loaded into the database.")
 
