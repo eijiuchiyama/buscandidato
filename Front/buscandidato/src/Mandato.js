90b91841422f912874f2666cb83b3a429c8a1bcc
João Mantovani <mantovanijoao@usp.br>
2024-11-17 18:08:33 +0000

    Atualização da página de mandato

--- Front/buscandidato/src/Mandato.js
+++ Front/buscandidato/src/Mandato.js
@@ -1,33 +1,33 @@
-import {Header} from './App.js'
+import {Header} from './App.js';
 import icone from './iconefoto.png';
+import Tabs from './components/Tabs.js';
 
 function Candidato(){
+    const tabData = [
+        { label: 'Despesas', content: 'Aqui estão as despesas detalhadas.' },
+        { label: 'Frentes', content: 'Aqui estão as frentes que ele participou.' },
+        { label: 'Votações', content: 'Aqui estão as votações em que ele participou.' },
+        { label: 'Proposições', content: 'Aqui estão as proposições que ele propôs.' },
+    ];
+
     return(
-    <html>
-      <head>
-      <link href=" https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css " rel="stylesheet"></link>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-      </head>
-      <body class="container p-3" style={{backgroundColor: '#d8d8d8'}}>
+        <html>
+        <head>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"></link>
+            <link href="components/tabs.css" rel="stylesheet"></link>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        </head>
+        <body class="container p-3" style={{backgroundColor: '#d8d8d8'}}>
         <Header />
-        <div class="card p-3">
-            <div class="card-header text-center rounded">
-                <h2>Partido Sem Nome</h2>
-            </div>
-            <div class="p-4">
-              <img src={icone} class="mx-auto d-block" style={{width:'30vw'}}/>
-            </div>
-            <div class="card-body">
-                <h3>Sigla do partido:</h3>
-                <h3>Ano de criação:</h3>
-                <h3>Presidente do partido:</h3>
-                <h3>Número de deputados federais:</h3>
-                <h3>Número de deputados senadores:</h3>
-                <h3>Número de vereadores na Câmara Municipal de São Paulo:</h3>
+        <div className="tabs-container">
+            <div className="App">
+                <h1 className="geeks">Fulano da Silva</h1>
+                <h2 className="geeks">Mandato Vereador SP 2020-2024</h2>
+                <Tabs tabs={tabData} />
             </div>
         </div>
-      </body>
-    </html>
+        </body>
+        </html>
     );
 }
 
