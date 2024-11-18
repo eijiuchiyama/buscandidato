be245a8170bba8074b2d6e3a92c304526c211e0a
ProgrammerMathematic <thiys@usp.br>
2024-11-18 18:51:42 -0300

    back: Adicionar urls de chamada da API para todas as tabelas no banco de dados

--- Back/buscandidatoapp/urls.py
+++ Back/buscandidatoapp/urls.py
@@ -2,7 +2,19 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/politicos/', views.politicos, name='politicos'),
-    path('api/partidos/', views.partidos, name='partidos'),
-    path('api/integrante_partido/', views.integrante_partido, name='integrante_partido')
+    path('api/politicos/', views.politico, name='politicos'),
+    path('api/partidos/', views.partido, name='partidos'),
+    path('api/orgaos/', views.orgao, name='orgaos'),
+    path('api/votacoes/', views.votacao, name='votacoes'),
+    path('api/frentes/', views.frente, name='frentes'),
+    path('api/proposicoes/', views.proposicao, name='proposicoes'),
+    path('api/despesas/', views.despesa, name='despesas'),
+    path('api/profissoes/', views.profissao, name='profissoes'),
+    path('api/mandatos/', views.mandato, name='mandatos'),
+    path('api/integrante_partido/', views.integrante_partido, name='integrante_partido'),
+    path('api/voto_politico/', views.voto_politico, name='voto_politico'),
+    path('api/votacao_proposicao/', views.votacao_proposicao, name='votacao_proposicao'),
+    path('api/autor_proposicao/', views.autor_proposicao, name='autor_proposicao'),
+    path('api/integrante_orgao/', views.integrante_orgao, name='integrante_orgao'),
+    path('api/integrante_frente/', views.integrante_frente, name='integrante_frente'),
 ]
--- Back/buscandidatoapp/views.py
+++ Back/buscandidatoapp/views.py
@@ -1,19 +1,79 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
 from django.core import serializers
-from .models import Politico, Partido, Integrante_Partido
+from .models import *
 
-def politicos(request):
+def politico(request):
     queryset = Politico.objects.all()
     data = serializers.serialize("json", queryset)
     return HttpResponse(data, content_type='application/json')
 
-def partidos(request):
+def partido(request):
     queryset = Partido.objects.all()
+    data = serializers.serialize("json", queryset)
+    return HttpResponse(data, content_type='application/json')
+
+def orgao(request):
+    queryset = Orgao.objects.all()
+    data = serializers.serialize("json", queryset)
+    return HttpResponse(data, content_type='application/json')
+
+def votacao(request):
+    queryset = Votacao.objects.all()
+    data = serializers.serialize("json", queryset)
+    return HttpResponse(data, content_type='application/json')
+
+def frente(request):
+    queryset = Frente.objects.all()
+    data = serializers.serialize("json", queryset)
+    return HttpResponse(data, content_type='application/json')
+
+def proposicao(request):
+    queryset = Proposicao.objects.all()
+    data = serializers.serialize("json", queryset)
+    return HttpResponse(data, content_type='application/json')
+
+def despesa(request):
+    queryset = Despesa.objects.all()
+    data = serializers.serialize("json", queryset)
+    return HttpResponse(data, content_type='application/json')
+
+def profissao(request):
+    queryset = Profissao.objects.all()
+    data = serializers.serialize("json", queryset)
+    return HttpResponse(data, content_type='application/json')
+
+def mandato(request):
+    queryset = Mandato.objects.all()
     data = serializers.serialize("json", queryset)
     return HttpResponse(data, content_type='application/json')
 
 def integrante_partido(request):
     queryset = Integrante_Partido.objects.all()
+    data = serializers.serialize("json", queryset)
+    return HttpResponse(data, content_type='application/json')
+
+def voto_politico(request):
+    queryset = Voto_Politico.objects.all()
+    data = serializers.serialize("json", queryset)
+    return HttpResponse(data, content_type='application/json')
+
+def votacao_proposicao(request):
+    queryset = Votacao_Proposicao.objects.all()
+    data = serializers.serialize("json", queryset)
+    return HttpResponse(data, content_type='application/json')
+
+def autor_proposicao(request):
+    queryset = Autor_Proposicao.objects.all()
+    data = serializers.serialize("json", queryset)
+    return HttpResponse(data, content_type='application/json')
+
+def integrante_orgao(request):
+    queryset = Integrante_Orgao.objects.all()
+    data = serializers.serialize("json", queryset)
+    return HttpResponse(data, content_type='application/json')
+
+def integrante_frente(request):
+    queryset = Integrante_Frente.objects.all()
     data = serializers.serialize("json", queryset)
     return HttpResponse(data, content_type='application/json')
